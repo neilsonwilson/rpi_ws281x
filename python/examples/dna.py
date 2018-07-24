6f99003febd09fb2fc4477c7a15399b8b30ae212
Neil Wilson <macadamiannut@gmail.com>
2018-07-24 00:45:55 +0000

    added reverse complement sequence

--- python/examples/dna.py
+++ python/examples/dna.py
@@ -23,12 +23,12 @@ def opt_parse():
                 signal.signal(signal.SIGTERM, signal_handler)
 
 # LED strip configuration:
-LED_COUNT      = 300      # Number of LED pixels.
+LED_COUNT      = 600      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 50   # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 100   # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
@@ -36,19 +36,24 @@ LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 
 # Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=5):
+def colorWipe(strip, color, wait_ms=1):
 	"""Wipe color across display a pixel at a time."""
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, color)
 		strip.setPixelColor(i-1,Color(0,0,0))
 		strip.show()
-		time.sleep(wait_ms/1000.0)
+		#time.sleep(wait_ms/1000.0)aa
 
-def dna(strip, dna_string, palette, wait_ms=200):
+def dna(strip, dna_string, palette, rev_palette, wait_ms=200):
 	"""DNA representation."""
+	 
 	for i in range(0, len(dna_string)):		
-		for j in range(0, strip.numPixels()):
-			strip.setPixelColor(j, palette[dna_string[(i + j) % len(dna_string)]])
+		for j in range(0, strip.numPixels()): 
+			if j < strip.numPixels()/2:
+                            strip.setPixelColor(j, palette[dna_string[(i + j) % len(dna_string)]])
+                        else:
+                            strip.setPixelColor(j, rev_palette[dna_string[(i - j +strip.numPixels()) % len(dna_string)]])
+                            
 		strip.show()
 		time.sleep(wait_ms/1000.0)
 
@@ -62,14 +67,28 @@ if __name__ == '__main__':
 	strip.begin()
 	
 	dna_palette = {
-		"G": Color(14, 180, 104),
-		"A": Color(87, 29, 177),
-		"T": Color(255, 230, 20),
-		"C": Color(255, 83, 20)
+		"G": Color(0, 255, 0),
+		"A": Color(255, 0, 0),
+		"T": Color(0, 255, 255),
+		"C": Color(0, 0, 255)
 	}
-	dna_string = "TGACGTGGAGGTGCATGTTTTTTTTTTTCTAGCTAGCGGCGATAGCGCGATCGATCGATCGCGCTATTTTTTTGCGTATCGGAGCTAGCGATCGAGCTAGCGGATCTTTTTTTGAGCTAGCGATCGAGATCTAGCGATCGAGCGCGCGCTAGCGTTTTTTTATCGACGTGTGATCGATCGAGTTCGAGCTAGCTAGCTAGCTGTTTTTTTCTAGGATCTGACGTGGAGGTGCATGCTAGCTATTTTTTTGCGGCGATAGCGCGATCGATCGATCGCGCTAGCGTATCGGAGCTAGCGATCGAGCTAGCGGATCGAGCTAGCGATCGAGATCTAGCGATCGAGCGCGCGCTAGCGATCGACGTGTGATCGATCGAGTTCGAGCTAGCTAGCTAGCTGCTAG"
+	
+	rev_dna_palette = {
+		"G": Color(0, 0, 255),
+		"A": Color(0, 255, 255),
+		"T": Color(255, 0, 0),
+		"C": Color(0, 255, 0)
+	}	
+	
+	
+	
+	
+	dna_string = "CATCACCATCACCATCACCATCACCATCACCATCACCATCACCATCACCATCACATGGTGAGCAAGGGCGAGGAGCTGTTCACCGGGGTGGTGCCCATCCTGGTCGAGCTGGACGGCGACGTAAACGGCCACAAGTTCAGCGTGTCTGGCGAGGGCGAGGGCGATGCCACCTACGGCAAGCTGACCCTGAAGTTCATCTGCACCACCGGCAAGCTGCCCGTGCCCTGGCCCACCCTCGTGACCACCCTGACCTACGGCGTGCAGTGCTTCAGCCGCTACCCCGACCACATGAAGCAGCACGACTTCTTCAAGTCCGCCATGCCCGAAGGCTACGTCCAGGAGCGCACCATCTTCTTCAAGGACGACGGCAACTACAAGACCCGCGCCGAGGTGAAGTTCGAGGGCGACACCCTGGTGAACCGCATCGAGCTGAAGGGCATCGACTTCAAGGAGGACGGCAACATCCTGGGGCACAAGCTGGAGTACAACTACAACAGCCACAACGTCTATATCATGGCCGACAAGCAGAAGAACGGCATCAAGGCGAACTTCAAGATCCGCCACAACATCGAGGACGGCAGCGTGCAGCTCGCCGACCACTACCAGCAGAACACCCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCACCCAGTCCGCCCTGAGCAAAGACCCCAACGAGAAGCGCGATCACATGGTCCTGCTGGAGTTCGTGACCGCCGCCGGGATCACTCTCGGCATGGACGAGCTGTACAAGTAA"
+	
+	
+	
 
 	print ('Press Ctrl-C to quit.')
 	while True:
 		print ('DNA animation.')
-		dna(strip, dna_string, dna_palette, 50)
+		dna(strip, dna_string, dna_palette, rev_dna_palette, 0.005)
